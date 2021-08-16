43eff89e5d0559e0daab4609af09c2f33d2f6ad9
Nathan de Silos Bernal Papavero <71854613+NathanSilos@users.noreply.github.com>
2021-08-16 20:04:56 -0300

    Add files via upload

---
+++ operadores.java
@@ -0,0 +1,20 @@
+class Main {
+  public static void main(String[] args) {
+    //testing operators
+
+    int valor1 = 200;
+    int valor2 = 100;
+
+    int soma = valor1 + valor2;
+    int subtracao = valor1 - valor2;
+    int multiplicacao = valor1 * valor2;
+    int divisao = valor1/valor2;
+    int resto = 10 % 4;
+
+
+    System.out.println("Soma: "+ soma);
+    System.out.println("Subtração: "+ subtracao);
+    System.out.println("Divisao: " + divisao);
+    System.out.println("Resto: "+ resto); 
+  }
+}
---
+++ variables.java
@@ -0,0 +1,28 @@
+class Main {
+  public static void main(String[] args) {
+
+    byte valor1 = 20;
+    System.out.println("Valor: " + valor1);
+
+    valor1 = 100;
+    System.out.println("Novo Valor: "+ valor1);
+
+    long valor2 = 10000000;
+    System.out.println("Novo Valor: "+ valor2);
+
+    double valor3 = 9.50110;
+    System.out.println("Novo Valor: "+ valor3);
+
+    float valor4= 12.684f;
+    System.out.println("Novo Valor: "+ valor4);
+
+    char valor5= 'f';
+    System.out.println("Novo Valor: "+ valor5);
+
+    boolean valor6 = false;
+    System.out.println("Novo Valor: "+ valor6);
+
+    String valor7 = "Nathan";
+    System.out.println("Novo Valor: " + valor7);
+  }
+}
